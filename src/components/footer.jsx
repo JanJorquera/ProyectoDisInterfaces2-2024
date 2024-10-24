05272bc78d8425ef04beb6cd490c565bbc3db7ac
JanJorquera <janjorquera@gmail.com>
2024-10-24 16:08:59 -0300

    Ajustes footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,8 +5,13 @@ import Instagram from '../assets/instagram.png';
 import Facebook from '../assets/facebook.png';
 import X from '../assets/x.png';
 import Whatsapp from '../assets/whatsapp.png';
+import UTFSM from '../assets/UTFSM.png';
 
 export const Footer = () => {
+    const redirect = (path, domain)=>{
+        window.location.href = `https://www.${path}.${domain}`;
+    }
+
     return (
         <div className='footer'>
             <div className='footer__element'>
@@ -44,14 +49,18 @@ export const Footer = () => {
             <div className='footer__element'>
                 <div className='footer__subelement'>Redes Sociales</div>
                 <div className='footer__imgcontainer'>
-                    <img className='footer__image' src={Facebook}></img>
-                    <img className='footer__image' src={Whatsapp}></img>
-                    <img className='footer__image' src={X}></img>
-                    <img className='footer__image' src={Instagram}></img>
+                    <div>
+                        <img onClick={()=>redirect('facebook', 'com')} className='footer__image' src={Facebook} />
+                        <img onClick={()=>redirect('web.whatsapp', 'com')} className='footer__image' src={Whatsapp} />
+                    </div>
+                    <div>
+                        <img onClick={()=>redirect('x', 'com')} className='footer__image' src={X} />
+                        <img onClick={()=>redirect('instagram', 'com')} className='footer__image' src={Instagram} />
+                    </div>
                 </div>
             </div>
             <div className='footer__element'>
-                Munidenuncias
+                <img onClick={()=>redirect('usm', 'cl')} className='footer__image footer__image--usm' src={UTFSM} />
             </div>
         </div>
     );
