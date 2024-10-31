31b129674cf4123de23ff85f4bb9c6cfff68b78d
unknown <hugovilazabriones@gmail.com>
2024-10-30 23:35:17 -0300

    report page update plus footer and navbar deleted from /

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 
 import NavBar from '../components/nav_bar';
 import UserTypeSelection from '../pages/user_type_selection.jsx';
-import HomePage from '../pages/home_page'
+import HomePage from '../pages/home_page';
 import LightbulbPage from '../pages/lightbulb_page';
 import MyReportsPage from '../pages/myreports_page.jsx';
 import DenunciarPage from '../pages/denunciar_page';
@@ -13,23 +13,29 @@ import Footer from '../components/footer';
 const Layout = () => {
   const [isLoged, setisLoged] = useState(false);
   const [userRut, setuserRut] = useState("");
+  const location = useLocation();
+
   return (
-    <BrowserRouter>
-      <div className='layout'>
-        <NavBar />
-        <div className='layout__page'>
-          <Routes>
-            <Route path='/' element={<UserTypeSelection />} />
-            <Route path='/home' element={<HomePage />} />
-            <Route path='/mis-denuncias' element={<MyReportsPage isLoged={isLoged} setisLoged={setisLoged} userRut={userRut} setuserRut={setuserRut}/>} />
-            <Route path='/denunciar' element={<DenunciarPage />} />
-            <Route path='/about' element={<AboutPage />} />
-          </Routes>
-        </div>
+    <div className='layout'>
+      {location.pathname !== '/' && <NavBar />}
+      <div className='layout__page'>
+        <Routes>
+          <Route path='/' element={<UserTypeSelection />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/mis-denuncias' element={<MyReportsPage isLoged={isLoged} setisLoged={setisLoged} userRut={userRut} setuserRut={setuserRut}/>} />
+          <Route path='/denunciar' element={<DenunciarPage />} />
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
       </div>
-      <Footer />
-    </BrowserRouter>
+      {location.pathname !== '/' && <Footer />}
+    </div>
   );
 }
 
-export default Layout;
+const App = () => (
+  <BrowserRouter>
+    <Layout />
+  </BrowserRouter>
+);
+
+export default App;
